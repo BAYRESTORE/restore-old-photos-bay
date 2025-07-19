15b106a0160d9e10db19c809207d42bb88bd6d8c
BAYRESTORE <prbayuu@gmail.com>
2025-07-19 21:20:54 +0700

    Create pages/index.js

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ display: "flex", minHeight: "100vh", alignItems: "center", justifyContent: "center", flexDirection: "column", fontFamily: "Arial" }}>
+      <h1>Restore Old Photos.Bay</h1>
+      <p>Selamat datang! Ini adalah halaman awal website restorasi foto AI.</p>
+    </div>
+  );
+}
