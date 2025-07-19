732eddb34f7b1ff362d210ea2e0b6391072a4076
BAYRESTORE <prbayuu@gmail.com>
2025-07-19 23:06:06 +0700

    Delete app directory

--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function HomePage() {
-  return (
-    <main style={{ textAlign: "center", padding: "50px" }}>
-      <h1>Restore Old Photos.Bay</h1>
-      <p>Situs AI untuk restorasi & edit foto otomatis</p>
-      <a href="/artroom" style={{ color: "blue" }}>Masuk ke ArtRoom</a>
-    </main>
-  );
-}
