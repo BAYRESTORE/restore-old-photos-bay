562e06eea0a13462243d3fc8b83d88f76a6c0b54
BAYRESTORE <prbayuu@gmail.com>
2025-07-19 22:41:50 +0700

    Create index.js

---
+++ artroom/index.js
@@ -0,0 +1,9 @@
+// pages/artroom/index.js
+export default function ArtRoom() {
+  return (
+    <div style={{ padding: '2rem' }}>
+      <h1>🎭 ArtRoom - Ganti Pakaian AI</h1>
+      <p>Unggah foto Anda dan pilih template pakaian seperti jas, kebaya, seragam, batik, dan lainnya.</p>
+    </div>
+  );
+}
